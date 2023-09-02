852bd787b471621e57c396bdfc96eada0bc6e44f
ted <theo_drz@live.com>
2023-09-01 22:12:05 -0400

    created seperate chatRooms and check for unique username

--- index.js
+++ index.js
@@ -1,42 +1,105 @@
-// Can we run the express app in a different port?  And then have the websocket and website run in the same app?
-// If we dont need express, then uninstall it
+// TO-DO
+// Clean up varibale names
+//    server => webSocket
+//    In client.js
+//    server => servername
+//    Consider rename index.js to server.js
+// Do we want to allow the user to be in multiple chats at once?
+//    - i.e. merge two seperate chat feeds into one
+
 const express = require('express');
 const WebSocket = require('ws');
 
-const port = 3000
+const webSocketPort = 8000
+const websitePort = 3000
 
-// Create home.html
-// Should allow user to choose a chatroom (i.e. port number !== 3000) and username
-// Server should make sure username is not already in use in the current chatroom
-//  - Add a users attr to the server, and run server.users.includes(username)
-const server = new WebSocket.Server({ port: port })
+// Is it possible to create a server for each individual chat room?
+// if we can pull this off, then we dont have to use the protocol string in this weird hacky way
+// To do this:
+//    create a /connect GET route (should act like an api route i.e. respond with JSON)
+//    request 'body' should contain username and servername
+//    create websocket and append to servers obj on request
+//    response should contain port
+//    on the client, use port from reponse to setup a new webSocket
+const server = new WebSocket.Server({ port: webSocketPort })
+
+// { servername: usersArray }
+const servers = {};
 
 server.on('connection', socket => {
-  // console.log('hello? UPDATED')
   // console.log(server.clients)
-  // console.log(socket.id)
+  // console.log(socket.test)
+  // console.log('CONNECTING')
+  // console.log(socket.protocol)
+
+  // Add user info to the servers obj
+  const i = socket.protocol.indexOf('-');
+  const servername = socket.protocol.slice(0, i);
+  const username = socket.protocol.slice(i + 1);
+  socket.username = username;
+  console.log(username, servername)
+  const currentServer = servers[servername];
+  // ADD TO SERVERS OBJECT HERE
+  if (currentServer) {
+    for (let i = 0; i < currentServer.length; i++) {
+      if (username === currentServer[i].username) {
+        socket.close();
+        return
+      }
+    }
+    currentServer.push(socket)
+  } else {
+    servers[servername] = [socket];
+  }
+  // console.log('DIDNT RETURN')
+  // servers[servername] = servers[servername] ? servers[servername].concat(socket) : servers[servername] = [socket]
+
+  // Send messages to the right chatRooms
   socket.on('message', message => {
     // console.log(message);
     // socket.send(message)
 
-    // Send message to all clients on this server
-    server.clients.forEach(clientSocket => {
-      clientSocket.send(message)
+    const data = JSON.parse(message.toString());
+    servers[data.server].forEach(socket => {
+      socket.send(message)
     })
+
+    // Send message to all clients on this server
+    // server.clients.forEach(clientSocket => {
+    //   console.log(clientSocket.protocol)
+    //   // clientSocket.send(message)
+    //   if (clientSocket.protocol === socket.protocol) {
+    //     clientSocket.send(message)
+    //   }
+    // })
   })
 })
 
 const app = express()
 app.use(express.static('pages'))
 app.get('/', (req, res) => {
-  // res.send('hello world')
   // res.sendFile(__dirname + '/client.html')
-  // console.log(__dirname)
   res.sendFile(__dirname + '/pages/client.html')
+  // res.sendFile(__dirname + '/pages/home.html')
 })
-app.listen(8000, () => {
-  console.log(`Running on port 8000`)
-})
-// app.listen(port, () => {
-//   console.log(`Running on port ${port}`)
+// app.get('/chat', (req, res) => {
+//   // SEND SERVER AND USERNAME IN REQ BODY, then use that info on server to sort users into chatrooms
+//   res.sendFile(__dirname + '/pages/client.html')
 // })
+// app.get('/connect', (req, res) => {
+//   const params = new URLSearchParams(req.url.slice(req.url.indexOf('?')))
+//   console.log(params)
+//   const username = params.get('username');
+//   const server = params.get('server');
+//   if (server in servers) {
+//     servers[server].push(username)
+//   } else {
+//     servers[server] = [username]
+//   }
+// 
+//   console.log(username, server)
+//   res.json('this is a message from the connect route')
+// })
+app.listen(websitePort, () => {
+  console.log(`Running on port ${websitePort}`)
+})
--- pages/client.js
+++ pages/client.js
@@ -1,25 +1,72 @@
-// console.log('HELLO FROM CLIENT.JS')
-const socket = new WebSocket('ws://localhost:3000')
+// THIS LINE MUST MATCH IN home.js
+// const socket = new WebSocket('ws://localhost:8000')
 // console.log(ws)
+let socket;
+let username = '';
+let server = '';
 
-socket.onmessage = ({ data }) => {
-  console.log(data)
-  data.text().then(data => {
-    // console.log(str)
-    const newMessage = JSON.parse(data);
-    const container = document.createElement('li')
-    container.textContent = newMessage.author + ': ' + newMessage.message;
-    document.getElementById('chatHistory').appendChild(container)
-  })
-}
+document.querySelector('#connect').addEventListener('click', (e) => {
+  e.preventDefault();
+  // const username = document.querySelector('#username').value
+  // const server = document.querySelector('#server').value
+  username = document.querySelector('#username').value
+  server = document.querySelector('#server').value
+  console.log(username, server);
+  // test = 
+  // socket = new WebSocket('ws://localhost:8000', JSON.stringify({ username, server }))
+  if (socket) {
+    socket.close();
+  }
+  socket = new WebSocket('ws://localhost:8000', `${server}-${username}`)
+  // socket = new WebSocket('ws://localhost:8000', ['one-two']);
 
-document.getElementById('send').addEventListener('click', () => {
+  socket.onmessage = ({ data }) => {
+    console.log(data)
+    data.text().then(data => {
+      // console.log(str)
+      const newMessage = JSON.parse(data);
+      const container = document.createElement('li')
+      container.textContent = newMessage.username + ': ' + newMessage.message;
+      document.querySelector('#chatHistory').appendChild(container)
+    })
+  }
+  // window.location.href = '/chat'
+
+  // fetch(path + '?' + new URLSearchParams(body), { method })
+  // fetch('/connect' + '?' + new URLSearchParams({ username, server }))
+  //   // .then(data => console.log(data))
+  //   .then(res => res.json())
+  //   .then(data => console.log(data))
+  
+  // Use a fetch request?
+  // If server returns true, go to chat
+  // Else return some error
+})
+
+// socket.onmessage = ({ data }) => {
+//   console.log(data)
+//   data.text().then(data => {
+//     // console.log(str)
+//     const newMessage = JSON.parse(data);
+//     const container = document.createElement('li')
+//     container.textContent = newMessage.author + ': ' + newMessage.message;
+//     document.getElementById('chatHistory').appendChild(container)
+//   })
+// }
+
+document.querySelector('#send').addEventListener('click', (e) => {
   // console.log(document.getElementById('newMessage').value)
   // socket.send('test message from client')
   // socket.send(document.getElementById('newMessage').value)
-  data = JSON.stringify({
-    message: document.getElementById('newMessage').value,
-    author: 'someAuthor'
-  })
-  socket.send(data)
+  e.preventDefault();
+  if (username && server) {
+    data = JSON.stringify({
+      message: document.querySelector('#newMessage').value,
+      username,
+      server,
+    })
+    socket.send(data)
+  } else {
+    console.log('you have no username or server selected')
+  }
 })
