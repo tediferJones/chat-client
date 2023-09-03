a01d044459c1851750c06b02042c3b2eece04516
ted <theo_drz@live.com>
2023-09-02 21:51:48 -0400

    added params to webSocket connection url, to avoid using the goofy protocol hack

--- index.js
+++ index.js
@@ -6,36 +6,46 @@
 //    Consider rename index.js to server.js
 // Do we want to allow the user to be in multiple chats at once?
 //    - i.e. merge two seperate chat feeds into one
+// Can we create a webSocket for each chatRoom?
+//    - Some ports may be blocked so maybe not a good idea
+//    - Cant send a checker request from client then act on that result
+//      - AS LONG AS we dont add the socket to servers[servername], their messages wont be sent to other users
+//      - user could just change the JS and force a return of true
+//    - So since we cant choose any port, and this bypasses username checking, it seems like a bad idea
+// IF YOU WANT TO GET RID OF WEIRD PROTOCOL MANIPULATION
+// USE THE WEBSOCKET TO COMMUNICATE USERNAME/SERVERNAME
+// from the client js, send this obj { username, servername }
+// then send a response to that specific client as to weather or not they are valid
+// If they are valid, THEN append event listener that will append new messages to the user
+
+// NEW STRATEGY FOR VERIFY CONNECTIONS
+// User sends username and password in url params on connection
+// Verify username is valid, and if so, add socket to server
+//
+// Try to remove all global variables from client.js, then the user cant manually change them?
 
 const express = require('express');
 const WebSocket = require('ws');
 
-const webSocketPort = 8000
+const webSocketPort = 8000 // Might need to use 443 in production
 const websitePort = 3000
 
-// Is it possible to create a server for each individual chat room?
-// if we can pull this off, then we dont have to use the protocol string in this weird hacky way
-// To do this:
-//    create a /connect GET route (should act like an api route i.e. respond with JSON)
-//    request 'body' should contain username and servername
-//    create websocket and append to servers obj on request
-//    response should contain port
-//    on the client, use port from reponse to setup a new webSocket
 const server = new WebSocket.Server({ port: webSocketPort })
 
 // { servername: usersArray }
 const servers = {};
 
-server.on('connection', socket => {
-  // console.log(server.clients)
-  // console.log(socket.test)
-  // console.log('CONNECTING')
-  // console.log(socket.protocol)
-
+server.on('connection', (socket, req) => {
   // Add user info to the servers obj
-  const i = socket.protocol.indexOf('-');
-  const servername = socket.protocol.slice(0, i);
-  const username = socket.protocol.slice(i + 1);
+  // console.log(socket)
+  console.log(new URLSearchParams(req.url))
+  // const searchParams = new URLSearchParams(req.url);
+  const params = new URLSearchParams(req.url.slice(req.url.indexOf('?')))
+  const username = params.get('username');
+  const servername = params.get('server');
+  // const i = socket.protocol.indexOf('-');
+  // const servername = socket.protocol.slice(0, i);
+  // const username = socket.protocol.slice(i + 1);
   socket.username = username;
   console.log(username, servername)
   const currentServer = servers[servername];
@@ -51,27 +61,17 @@ server.on('connection', socket => {
   } else {
     servers[servername] = [socket];
   }
-  // console.log('DIDNT RETURN')
-  // servers[servername] = servers[servername] ? servers[servername].concat(socket) : servers[servername] = [socket]
+  // idk = JSON.stringify({ test: 'validationTest' })
+  // socket.send(new Blob([idk], {
+  //   type: "text/plain"
+  // }))
 
   // Send messages to the right chatRooms
   socket.on('message', message => {
-    // console.log(message);
-    // socket.send(message)
-
     const data = JSON.parse(message.toString());
     servers[data.server].forEach(socket => {
       socket.send(message)
     })
-
-    // Send message to all clients on this server
-    // server.clients.forEach(clientSocket => {
-    //   console.log(clientSocket.protocol)
-    //   // clientSocket.send(message)
-    //   if (clientSocket.protocol === socket.protocol) {
-    //     clientSocket.send(message)
-    //   }
-    // })
   })
 })
 
@@ -82,23 +82,36 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/pages/client.html')
   // res.sendFile(__dirname + '/pages/home.html')
 })
-// app.get('/chat', (req, res) => {
-//   // SEND SERVER AND USERNAME IN REQ BODY, then use that info on server to sort users into chatrooms
-//   res.sendFile(__dirname + '/pages/client.html')
-// })
+function usernameIsValid(sockets, username) {
+  if (sockets === undefined) return true
+  for (let i = 0; i < sockets.length; i++) {
+    if (username === sockets[i].username) {
+      return false;
+    }
+  }
+  return true;
+}
 // app.get('/connect', (req, res) => {
 //   const params = new URLSearchParams(req.url.slice(req.url.indexOf('?')))
 //   console.log(params)
 //   const username = params.get('username');
 //   const server = params.get('server');
-//   if (server in servers) {
-//     servers[server].push(username)
-//   } else {
-//     servers[server] = [username]
+//   const currentServer = servers[server]
+//   let connected = false;
+//   console.log(currentServer)
+//   console.log(usernameIsValid(currentServer, username))
+//   if (usernameIsValid(currentServer, username)) {
+//     connected = true;
+//     // if (currentServer) {
+//     //   currentServer.push(socket)
+//     // } else {
+//     //   servers[server] = [socket];
+//     // }
 //   }
-// 
-//   console.log(username, server)
-//   res.json('this is a message from the connect route')
+//   // res.json('this is a message from the connect route')
+//   res.json({
+//     connected
+//   })
 // })
 app.listen(websitePort, () => {
   console.log(`Running on port ${websitePort}`)
--- pages/client.js
+++ pages/client.js
@@ -7,58 +7,44 @@ let server = '';
 
 document.querySelector('#connect').addEventListener('click', (e) => {
   e.preventDefault();
-  // const username = document.querySelector('#username').value
-  // const server = document.querySelector('#server').value
+
   username = document.querySelector('#username').value
   server = document.querySelector('#server').value
   console.log(username, server);
-  // test = 
-  // socket = new WebSocket('ws://localhost:8000', JSON.stringify({ username, server }))
+
+  // const serverRes = fetch('/connect' + '?' + new URLSearchParams({ username, server }))
+  //   .then(res => res.json())
+  //   .then(data => console.log(data))
+
+  // WORKING
   if (socket) {
     socket.close();
   }
-  socket = new WebSocket('ws://localhost:8000', `${server}-${username}`)
-  // socket = new WebSocket('ws://localhost:8000', ['one-two']);
+  // socket = new WebSocket('ws://localhost:8000', `${server}-${username}`)
+  socket = new WebSocket('ws://localhost:8000?' + new URLSearchParams({ username, server }))
+  console.log(socket)
+  // SEND VALIDATION AND CHECK RESULT HERE
+
+  // socket.onopen = (idk) => {
+  //   console.log(idk)
+  // }
 
   socket.onmessage = ({ data }) => {
-    console.log(data)
+    // console.log(data)
     data.text().then(data => {
-      // console.log(str)
       const newMessage = JSON.parse(data);
       const container = document.createElement('li')
       container.textContent = newMessage.username + ': ' + newMessage.message;
       document.querySelector('#chatHistory').appendChild(container)
     })
   }
-  // window.location.href = '/chat'
 
-  // fetch(path + '?' + new URLSearchParams(body), { method })
-  // fetch('/connect' + '?' + new URLSearchParams({ username, server }))
-  //   // .then(data => console.log(data))
-  //   .then(res => res.json())
-  //   .then(data => console.log(data))
-  
-  // Use a fetch request?
-  // If server returns true, go to chat
-  // Else return some error
+  // APPEND A BUTTON WITH EVENTLISTENER THAT WILL TRIGGER socket.close()
 })
 
-// socket.onmessage = ({ data }) => {
-//   console.log(data)
-//   data.text().then(data => {
-//     // console.log(str)
-//     const newMessage = JSON.parse(data);
-//     const container = document.createElement('li')
-//     container.textContent = newMessage.author + ': ' + newMessage.message;
-//     document.getElementById('chatHistory').appendChild(container)
-//   })
-// }
-
 document.querySelector('#send').addEventListener('click', (e) => {
-  // console.log(document.getElementById('newMessage').value)
-  // socket.send('test message from client')
-  // socket.send(document.getElementById('newMessage').value)
   e.preventDefault();
+
   if (username && server) {
     data = JSON.stringify({
       message: document.querySelector('#newMessage').value,
