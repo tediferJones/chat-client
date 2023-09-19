35b7b0e077925c424fd0809d157b21fc59ae3d68
ted <theo_drz@live.com>
2023-09-18 22:27:35 -0400

    minor changes, cleaned up server file

--- bun-V2/src/components/testComponent.tsx
+++ bun-V2/src/components/testComponent.tsx
@@ -6,7 +6,7 @@ export default function TestComponent({ message }: { message: string }) {
   // <button onClick={() => console.log('Is this a working component?')}>INCREMENT</button>
   return (
     <div>
-      <h1 className='bg-green-900 p-4 flex justify-center'>TEST CLASS NAMES</h1>
+      <h1 className='bg-green-900 p-4 flex justify-center text-white'>TEST CLASS NAMES</h1>
       <h1>Hello {message}</h1>
       <h1>Counter: {state}</h1>
       <button onClick={() => setState(state + 1)}>INCREMENT</button>
--- bun-V2/src/index.tsx
+++ bun-V2/src/index.tsx
@@ -1,23 +1,11 @@
 import { renderToReadableStream } from 'react-dom/server';
 
 // EXAMPLE: https://www.npmjs.com/package/@bun-examples/react-ssr
-// [ DONE ] Figure out what the bottom half of dev.tsx from example does
-// [ DONE ] Figure out how to serve JS files when a JS file is requested
 // [ THIS IS THE FIX ] Consider creating a layout file, maybe thats part of what is causing issues
-// [ DONE ] Try to create another page with a seperate component
-// [ DONE ] Get index page working
-// [ DONE ] Try to install tailwind, https://stackoverflow.com/questions/72919826/how-to-use-tailwind-with-bun
-// [ DONE ] the build folder can probably be added to .gitignore
-// [ DONE ] fix paths in this file so that it can be run from the root of the project
-//    - [ DONE ] Might be a good idea to also create a start script in package.json
 // Consider using tsconfig path aliases, make on for @root = './', @components = './src/components'
-// [ DONE ] Add error handling, what if users manually naviages /thisFileDoesntExist
-// [ DONE ] Get API routes working
-// [ DONE ] Get all pages and apiRoutes imported when server starts, dont import them dynamically
 
 // All paths are based on the location of this file (the file that runs the server)
 const rootPath = import.meta.dir.replace('src', '');
-// console.log(rootPath)
 
 const srcRouter = new Bun.FileSystemRouter({
   dir: rootPath + 'src/pages',
@@ -65,76 +53,20 @@ Object.keys(apiRouter.routes).forEach(async (path) => {
 const server = Bun.serve({
   port: 3000,
   async fetch(req) {
-    // const srcMatch = srcRouter.match(req);
-    // if (srcMatch) {
-    //   console.log('MATCH FROM SRC ROUTER')
-    //   console.log(srcMatch.src)
-    // }
     const builtMatch = buildRouter.match(req)
     const apiMatch = apiRouter.match(req)
     const srcMatch = srcRouter.match(req)
 
-    // console.log("REQ URL:")
-    // console.log(req.url)
-    // console.log(buildRouter)
-    // If request is for a page, then return page component
-    // console.log(apiRouter)
-    // if (apiMatch) {
-    //   console.log('API ROUTE DETECTED')
-    // }
-
-    // if (builtMatch) {
     if (srcMatch && builtMatch) {
       console.log('requesting page')
-      // console.log('MATCH FROM BUILT ROUTER')
-      // console.log(builtMatch.src)
-      // console.log(builtMatch)
-      // console.log('SRC ROUTER')
-      // const srcMatch = srcRouter.match(req)
-
-      // if (!srcMatch) {
-      //   return new Response('Error, no source page found')
-      // }
-      // console.log('PAGE MATCHES')
-      // console.log(srcRouter)
-      // console.log(buildRouter)
-      // console.log(srcMatch)
-      // console.log(builtMatch)
-      // console.log(srcMatch.src)
-      // console.log(builtMatch.src)
-
-      // const PageToRender = await import('./pages/' + builtMatch.src)
-      // const PageToRender = await import(srcMatch.filePath)
-      // const stream = await renderToReadableStream(<PageToRender.default />, {
-      //   // PATH TO PAGE IN BUILD FOLDER
-      //   // bootstrapScriptContent: `globalThis.PATH_TO_PAGE = "/${builtMatch.src}";`,
-      //   bootstrapScriptContent: `globalThis.PATH_TO_PAGE = "/${builtMatch.src}";`,
-      //   // bootstrapScriptContent: 'console.log("LOADED")',
-
-      //   bootstrapModules: ['/hydrate.js'],
-      // })
-
-      // const PageToRender = pages[builtMatch.pathname]
-      // // console.log(typeof PageToRender)
-      // console.log(PageToRender)
       // const stream = await renderToReadableStream(<PageToRender.default />, {
       const stream = await renderToReadableStream(pages[builtMatch.pathname].default(), {
         bootstrapScriptContent: `globalThis.PATH_TO_PAGE = "/${builtMatch.src}";`,
         bootstrapModules: ['/hydrate.js'],
       });
-      // return new Response(stream, {
-      //   headers: { 'Content-Type': 'text/html' }
-      // })
+
       return new Response(stream);
     } else if (apiMatch) {
-      // Import script based in apiMatch, just like for builtMatch
-      // Use req.method to select the correct function from the file
-      // console.log(apiMatch);
-      // console.log(apiRoutes)
-      // const apiRoute = await import('./apiRoutes/' + apiMatch.src)
-      // const apiRoute = await import(apiMatch.filePath)
-      // return apiRoute[req.method](req);
-
       return apiRoutes[apiMatch.pathname][req.method](req)
     } else {
       console.log(`REQUESTING FILE`)
