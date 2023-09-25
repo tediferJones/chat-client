9c851cb94898acd58dd8b02a3b6640e789fbd3cd
ted <theo_drz@live.com>
2023-09-24 22:27:00 -0400

    moved bun-V3 to notes folder

--- bun-V2/src/components/Greeter.tsx
+++
@@ -1,7 +0,0 @@
-export default function Greeter({ username }: { username: string }) {
-  return (
-    <div className='flex justify-center p-8 m-2 bg-purple-400 text-2xl text-white font-bold'>
-      <h1>Hello, {username}</h1>
-    </div>
-  )
-}
--- bun-V2/src/hydrate.tsx
+++
@@ -1,4 +0,0 @@
-import { hydrateRoot } from "react-dom/client";
-const { default: App } = await import(globalThis.PATH_TO_PAGE);
-
-hydrateRoot(document, <App />);
